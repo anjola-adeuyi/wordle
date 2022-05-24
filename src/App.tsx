174b3a7c14926983c34b619e820ba937666bcac4
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-05-24 16:59:37 +0100

    setup fetch json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/solutions')
+      .then((res) => res.json())
+      .then((json) => {
+        // random int btw 0 & 14
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
+  console.log(solution);
+
   return (
     <div className='App'>
       <h1>Wordle</h1>
+      {solution ? <h2>{solution}</h2> : <h2>Loading...</h2>}
     </div>
   );
 }
